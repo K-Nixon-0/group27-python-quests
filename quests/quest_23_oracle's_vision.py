72170a94bdf8f655cc25acbd4cd8e8f7d243ed9b
Luxe-Gwiz <l.gayawira@alustudent.com>
2026-06-29 19:55:08 +0200

    fixing quest 23 and quest 27

---
+++ quests/quest_23_oracle's_vision.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def calculate_area(length, width):
+    return length * width
+
+area1 = calculate_area(5, 10)
+print(f"Rectangle 1 area: {area1}")
+
+area2 = calculate_area(7, 3)
+print(f"Rectangle 2 area: {area2}")
