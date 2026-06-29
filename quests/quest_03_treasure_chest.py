60129a9476033b595248e3fe077ac77831774149
K-Nixon-0 <n.kami@alustudent.com>
2026-06-29 10:51:03 +0200

    Add Python Quest solutions: Treasure Chest, Potion Brewer, Maze of Many Choices, Countdown, Personalized Scroll, and Code Breaker

---
+++ quests/quest_03_treasure_chest.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+age = 27
+favorite_food = "Fufu with beans soup"
+print(f"I am {age} years old and my favorite food is {favorite_food}.")
---
+++ quests/quest_13_maze_of_many_choices.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+score = int(input("Enter your score (0-100): "))
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+else:
+    print("Needs Improvement")
+    print("Otherwise He/she will be demoted")
