8d9f29f70241311bade3e2ce3e64fb6c6ac91ebe
ynshuti-cloud <y.nshuti@alustudent.com>
2026-06-29 22:28:05 +0200

    Create   quest_06_fortune_teller.py

---
+++ quests/quest_06_fortune_teller.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+name = input("What is your name? ")
+quest = input("What is your quest? ")
+
+print(f"Greetings, {name}! Your quest is '{quest}'. Good luck on your code journey!")
