f77ab6ea9b920faf3ee8e44c418528141bace793
aalier1-dot <a.alier1@alustudent.com>
2026-07-05 12:01:09 -0700

    Update quest 16 marching ants with comments

--- quests/quest_16_marching_ants.py
+++ quests/quest_16_marching_ants.py
@@ -1,3 +1,7 @@
-git add quests/quest_16_marching_ants.py
-git commit -m "Update quest 16 marching ants"
+#!/usr/bin/python3
+# Quest 16: The Marching Ants
+# Using a for loop to print numbers from 1 to 10
+for i in range(1, 11):
+    # Print each number on a new line
+    print(i)
 
